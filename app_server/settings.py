3a732a27281bef4f597f20d63073e399bd582aaf
JIGUN08 <skycenter205@gmail.com>
2025-11-05 13:23:45 +0900

    Update settings.py

--- app_server/settings.py
+++ app_server/settings.py
@@ -65,9 +65,7 @@ MIDDLEWARE = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://*.example.com',
     'http://localhost:8000',
-    'http://localhost:8000', 
     'http://127.0.0.1:8000',
     'http://127.0.0.1',
     'https://*.onrender.com'
@@ -78,7 +76,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "http://10.0.2.2:8000", # Android 에뮬레이터 IP
+    "http://10.0.2.2:8000",
 ]
 
 REST_FRAMEWORK = {
